6a53385b4f5b6bd5342e9d2f1acf13bdc2ed32d1
Charlie Greenman <charlie@razroo.com>
2023-10-10 10:00:49 -0400

    Add code for angular-16.2.0 integrate-ngrx-store.
    [Url: razroo.com/community/angular-16.2.0/add-ngrx-store-infrastructure/integrate-ngrx-store]

--- apps/devgen-test-ten/src/app/app.module.ts
+++ apps/devgen-test-ten/src/app/app.module.ts
@@ -8,10 +8,16 @@ import { GlobalHeaderComponentModule } from "@devgen-test-ten/common/ui";
 import { GlobalFooterModule } from "@devgen-test-ten/common/ui";
 import { GlobalSidenavModule } from "@devgen-test-ten/common/ui";
 import { MatListModule } from '@angular/material/list';
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { StoreModule } from "@ngrx/store";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule, StoreModule.forRoot({}), StoreDevtoolsModule.instrument({
+    maxAge: 25, // Retains last 25 states
+    logOnly: environment.production, // Restrict extension to log-only mode
+    autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+  }),],
   providers: [],
   bootstrap: [AppComponent],
 })
